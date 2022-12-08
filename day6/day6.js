dc19afbd8dc2f72ee5168666dcd14c6e8374ba34
Diego Calderon <dacf9x@gmail.com>
2022-12-08 14:10:58 -0600

    Day 6

--- day6/day6.js
+++ day6/day6.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+/*const fs = require('fs');
 
 const file = fs.readFileSync('./day6/06-06.txt');
 const data = file.toString();
@@ -40,6 +40,30 @@ const toFindDuplicates2 = (array) => {
 }
 fourteenCharacters.forEach(element => toFindDuplicates2(element));
 fourCharacters.forEach(element => toFindDuplicates1(element));
-
+*/
 
 debugger;
+
+const fs = require('fs');
+
+const data = fs.readFileSync('./day6/06-06.txt', 'utf-8').trim();
+
+const findUniques = (length) => {
+    const list = [];
+    let count = 0;
+    for (let next of data.split('')) {
+        list.push(next);
+        count++;
+        // If we have less than the length we need to keep going
+        if (list.length < length) continue;
+        // If we have the length we need to check if we have a unique set
+        if (new Set(list).size === length) break;
+        list.shift();
+    }
+    return count;
+};
+
+// part 1
+console.log(findUniques(4));
+// part 2
+console.log(findUniques(14));
